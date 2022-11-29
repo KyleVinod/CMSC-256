2e2cb2e203812079ac42c26ad5c8ef7855274c74
Kyle Vinod <89536613+KyleVinod@users.noreply.github.com>
2022-11-29 17:35:13 -0500

    Add files via upload

---
+++ Labs/Lab 3/ActorComparator.java
@@ -0,0 +1,11 @@
+package cmsc256;
+import bridges.data_src_dependent.ActorMovieIMDB;
+
+import java.util.Comparator;
+
+public class ActorComparator implements Comparator<ActorMovieIMDB> {
+    public int compare(ActorMovieIMDB name1, ActorMovieIMDB name2) {
+        int compare = name1.getActor().compareTo(name2.getActor());
+        return compare;
+    }
+}
